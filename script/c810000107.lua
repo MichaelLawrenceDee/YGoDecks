399ca84619bf1f953dfe53a35e0665ede5e1718e
Michael Lawrence C. Dee <michael_lawrence_dee@yahoo.com>
2015-12-29 14:31:54 +0800

    12/29/2015 2:31 pm update

--- script/c810000107.lua
+++ script/c810000107.lua
@@ -1,61 +1,102 @@
 --Frightfur Backup
 function c810000107.initial_effect(c)
-	--Activate
+	--
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+	e1:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
 	e1:SetCondition(c810000107.condition)
 	e1:SetTarget(c810000107.target)
-	e1:SetOperation(c810000107.activate)
+	e1:SetOperation(c810000107.operation)
 	c:RegisterEffect(e1)
 	--
 	local e2=e1:Clone()
 	e2:SetCode(EVENT_CHAINING)
-	e2:SetCondition(c810000107.con2)
+	e2:SetCondition(c810000107.condition2)
 	c:RegisterEffect(e2)
 end
 function c810000107.condition(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetCurrentPhase()==PHASE_BATTLE
-end
-function c810000107.cfilter(c,tp)
-	return c:IsOnField() and c:IsType(TYPE_MONSTER) and c:IsSetCard(0xad) and c:IsControler(tp)
-end
-function c810000107.con2(e,tp,eg,ep,ev,re,r,rp)
-	local ex,tg,tc=Duel.GetOperationInfo(ev,CATEGORY_DESTROY)
-	return Duel.GetCurrentPhase()~=PHASE_BATTLE and ex and tg~=nil and tc+tg:FilterCount(c810000107.cfilter,nil,tp)-tg:GetCount()>0
-end
-function c810000107.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c810000107.cfilter,tp,LOCATION_MZONE,0,1,nil) end
-end
-function c810000107.activate(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-	local g=Duel.SelectMatchingCard(tp,c810000107.cfilter,tp,LOCATION_MZONE,0,1,1,nil)
-	local tc=g:GetFirst()
-	if tc then
-		Duel.HintSelection(g)
-		local e1=Effect.CreateEffect(e:GetHandler())
-		e1:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_DESTROY_REPLACE)
-		e1:SetCountLimit(1)
-		e1:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
-		e1:SetTarget(c810000107.reptg)
-		e1:SetOperation(c810000107.repop)
-		e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
-		tc:RegisterEffect(e1)
+	local tc=Duel.GetAttacker()
+	local bc=tc:GetBattleTarget()
+	if tc:IsControler(1-tp) then
+		tc=Duel.GetAttackTarget()
+		bc=Duel.GetAttacker()
+	end
+	if not tc or not bc or tc:IsHasEffect(EFFECT_INDESTRUCTABLE_BATTLE) then return false end
+	if not tc:IsSetCard(0xad) then return false end
+	e:SetLabelObject(tc)
+	if bc==Duel.GetAttackTarget() and bc:IsDefencePos() then return false end
+	if bc:IsPosition(POS_FACEUP_DEFENCE) and bc==Duel.GetAttacker() then
+		if not bc:IsHasEffect(EFFECT_DEFENCE_ATTACK) then return false end
+		if bc:IsHasEffect(EFFECT_DEFENCE_ATTACK) then
+			if bc:GetEffectCount(EFFECT_DEFENCE_ATTACK)==1 then
+				if tc:IsAttackPos() then
+					if bc:GetDefence()==tc:GetAttack() and not bc:IsHasEffect(EFFECT_INDESTRUCTABLE_BATTLE) then
+						return bc:GetDefence()~=0
+					else
+						return bc:GetDefence()>=tc:GetAttack()
+					end
+				else
+					return bc:GetDefence()>tc:GetDefence()
+				end
+			elseif bc:IsHasEffect(EFFECT_DEFENCE_ATTACK) then
+				if tc:IsAttackPos() then
+					if bc:GetAttack()==tc:GetAttack() and not bc:IsHasEffect(EFFECT_INDESTRUCTABLE_BATTLE) then
+						return bc:GetAttack()~=0
+					else
+						return bc:GetAttack()>=tc:GetAttack()
+					end
+				else
+					return bc:GetAttack()>tc:GetDefence()
+				end
+			end
+		end
+	else
+		if tc:IsAttackPos() then
+			if bc:GetAttack()==tc:GetAttack() and not bc:IsHasEffect(EFFECT_INDESTRUCTABLE_BATTLE) then
+				return bc:GetAttack()~=0
+			else
+				return bc:GetAttack()>=tc:GetAttack()
+			end
+		else
+			return bc:GetAttack()>tc:GetDefence()
+		end
 	end
 end
-function c810000107.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	return true
+function c810000107.cfilter(c,e,tp)
+	return c:IsOnField() and c:IsType(TYPE_MONSTER) and c:IsControler(tp) and (not e or c:IsRelateToEffect(e)) 
+		and c:IsSetCard(0xad)
 end
-function c810000107.repop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_UPDATE_ATTACK)
-	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e1:SetReset(RESET_EVENT+0x1fe0000)
-	e1:SetValue(800)
-	c:RegisterEffect(e1)
+function c810000107.condition2(e,tp,eg,ep,ev,re,r,rp)
+	local ex,tg,tc=Duel.GetOperationInfo(ev,CATEGORY_DESTROY)
+	if tg==nil then return false end
+	local g=tg:Filter(c810000107.cfilter,nil,nil,tp)
+	g:KeepAlive()
+	e:SetLabelObject(g)
+	return ex and tc+g:GetCount()-tg:GetCount()==1
+end
+function c810000107.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=e:GetLabelObject()
+	if chk==0 then return g end
+	Duel.SetTargetCard(g)
+end
+function c810000107.operation(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+		e1:SetValue(1)
+		e1:SetReset(RESET_PHASE+PHASE_DAMAGE)
+		tc:RegisterEffect(e1)
+		local e2=e1:Clone()
+		e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+		e2:SetReset(RESET_CHAIN)
+		tc:RegisterEffect(e2)
+		local e3=Effect.CreateEffect(e:GetHandler())
+		e3:SetType(EFFECT_TYPE_SINGLE)
+		e3:SetCode(EFFECT_UPDATE_ATTACK)
+		e3:SetReset(RESET_EVENT+0x1fe0000)
+		e3:SetValue(800)
+		tc:RegisterEffect(e3)
+	end
 end
