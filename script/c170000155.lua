9e7b30440e948d223fe037a1a9475663cb3dcffe
Michael Lawrence C. Dee <michael_lawrence_dee@yahoo.com>
2016-01-31 11:15:10 +0800

    1/31/16 11:14 am update

--- script/c170000155.lua
+++ script/c170000155.lua
@@ -23,14 +23,14 @@ function c170000155.filter(c)
 	return c:IsRace(RACE_DRAGON) and c:IsFaceup()
 end
 function c170000155.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and c170000155.filter(chkc,e) end
-	if chk==0 then return Duel.IsExistingTarget(c170000155.filter,tp,LOCATION_MZONE,0,1,nil) end
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and c170000155.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c170000155.filter,tp,LOCATION_MZONE,0,1,e:GetHandler()) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	Duel.SelectTarget(tp,c170000155.filter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SelectTarget(tp,c170000155.filter,tp,LOCATION_MZONE,0,1,1,e:GetHandler())
 end
 function c170000155.operation(e,tp,eg,ep,ev,re,r,rp)
    local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
+	if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() then
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
